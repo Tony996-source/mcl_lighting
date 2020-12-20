913e6c013dd44d732bcd563d49eaaad395eb2847
Tony996-source <62044609+Tony996-source@users.noreply.github.com>
2020-12-20 03:23:58 +0000

    update

--- crafts.lua
+++ crafts.lua
@@ -1,25 +1,25 @@
  -- Crafts
 
 minetest.register_craft({
-  output = "lighting:blue_lantern_f",
+  output = "mcl_lighting:blue_lantern_f",
   recipe = {
-    {"", "mcl_core:iron_ingot", ""},
-    {"mcl_core:glass_blue", "mcl_core:torch", "mcl_core:glass_blue"},
-    {"mcl_core:stick", "mcl_core:iron_ingot", "mcl_core:stick"}
+    {"", "mcl_core:iron_nugget", ""},
+    {"mcl_core:glass_blue", "mcl_torches:torch", "mcl_core:glass_blue"},
+    {"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"}
   }
 })
 
 minetest.register_craft({
-  output = "lighting:lantern_f",
+  output = "mcl_lighting:lantern_f",
   recipe = {
-    {"", "mcl_core:iron_ingot", ""},
-    {"mcl_core:glass_orange", "mcl_core:torch", "mcl_core:glass_orange"},
-    {"mcl_core:stick", "mcl_core:iron_ingot", "mcl_core:stick"}
+    {"", "mcl_core:iron_nugget", ""},
+    {"mcl_core:glass_orange", "mcl_torches:torch", "mcl_core:glass_orange"},
+    {"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"}
   }
 })
 
 minetest.register_craft({
-	output = "lighting:light_rod 4",
+	output = "mcl_lighting:light_rod 4",
 	recipe = {
 		{"mcl_mobitems:blaze_rod"},
 		{"mcl_mobitems:blaze_rod"},
