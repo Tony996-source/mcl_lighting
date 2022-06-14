9c47eed7b85167ac89df3f513e752640d5d65d75
Tony996-source <62044609+Tony996-source@users.noreply.github.com>
2022-06-14 13:29:41 +0100

    update

--- crafts.lua
+++ crafts.lua
@@ -3,10 +3,10 @@
  minetest.register_craft({
 	output = "mcl_lighting:modern_light_rod 4",
 	recipe = {
-		{"mcl_core:glass", "", "mcl_core:glass"}
+		{"mcl_core:glass", "", "mcl_core:glass"},
 		{"mcl_core:glass","mcl_nether:glowstone_dust", "mcl_core:glass"},
 		{"mcl_core:glass", "mcl_core:iron_ingot", "mcl_core:glass"},
-	},
+	}
 })
 
 minetest.register_craft({
@@ -14,7 +14,7 @@ minetest.register_craft({
   recipe = {
     {"", "mcl_core:iron_nugget", ""},
     {"mcl_core:glass_blue", "mcl_nether:glowstone_dust", "mcl_core:glass_blue"},
-    {"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"}
+    {"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
   }
 })
 
@@ -23,7 +23,7 @@ minetest.register_craft({
   recipe = {
     {"", "mcl_core:iron_nugget", ""},
     {"mcl_core:glass_orange", "mcl_nether:glowstone_dust", "mcl_core:glass_orange"},
-    {"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"}
+    {"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
   }
 })
 
@@ -49,7 +49,7 @@ minetest.register_craft({
   output = "mcl_lighting:barlight_c 4",
   recipe = {
     {"mcl_core:iron_ingot", "mcl_nether:glowstone_dust", "mcl_core:iron_ingot"},
-    {"mcl_core:gold_ingot", "mcl_core:glass", "mcl_core:gold_ingot"}
+    {"mcl_core:gold_ingot", "mcl_core:glass", "mcl_core:gold_ingot"},
   }
 })
 
@@ -69,7 +69,7 @@ minetest.register_craft({
   output = "mcl_lighting:glowlight_modern",
   recipe = {
     {"mcl_core:iron_ingot", "mcl_nether:glowstone_dust", "mcl_core:iron_ingot"},
-    {"", "mcl_core:glass", ""}
+    {"", "mcl_core:glass", ""},
   }
 })
 
@@ -78,7 +78,7 @@ minetest.register_craft({
   recipe = {
     {"mcl_dye:white", "mcl_core:glass", ""},
     {"mcl_core:glass", "mcl_nether:glowstone_dust", "mcl_core:iron_ingot"},
-    {"", "mcl_dye:grey", "mcl_core:iron_ingot"}
+    {"", "mcl_dye:grey", "mcl_core:iron_ingot"},
   }
 })
 
@@ -87,7 +87,7 @@ minetest.register_craft({
   recipe = {
     {"mcl_wool:grey", "mcl_nether:glowstone_dust", "mcl_wool:grey"},
     {"", "mcl_core:iron_ingot", ""},
-    {"", "mcl_core:iron_ingot", ""}
+    {"", "mcl_core:iron_ingot", ""},
   }
 })
 
@@ -96,6 +96,6 @@ minetest.register_craft({
   recipe = {
     {"mcl_wool:white", "mcl_nether:glowstone_dust", "mcl_wool:white"},
     {"", "mcl_core:iron_ingot", ""},
-    {"", "mcl_core:iron_ingot", ""}
+    {"", "mcl_core:iron_ingot", ""},
   }
 })
